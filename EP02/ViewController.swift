383fa6ac05ccf04c1bef49f0efc9d7e37d263e48
David <garyprado45@gmail.com>
2021-05-21 17:41:04 -0400

    Diseño

---
+++ EP02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EP02
+//
+//  Created by user191022 on 5/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
